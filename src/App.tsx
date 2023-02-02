027d90c3551de39ff2c522e6a74047939954a25f
Ian Macartney <ian@convex.dev>
2023-02-01 18:44:31 -0800

    store game id in hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Id } from "../convex/_generated/dataModel";
 import { useQuery } from "../convex/_generated/react";
 import "./App.css";
 import Game from "./Game";
 import GameRound from "./GameRound";
 import { useSessionMutation } from "./hooks/sessionsClient";
 
+const ConvexIdLength = 22;
+
 function App() {
   const hostGame = useSessionMutation("game:create");
+  const [gameId, setGameId] = useState(() => {
+    if (typeof window === "undefined") return null;
+    console.log(window.location.hash);
+    const id = window.location.hash.substring(1);
+    if (!id || id.length !== 22) return null;
+    console.log({ id });
+    return new Id("games", id);
+  });
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    if (gameId) window.location.hash = gameId.id;
+  }, [gameId]);
   const [gameCode, setGameCode] = useState("");
   const joinGame = useSessionMutation("game:join");
   const roundId = useQuery("publicGame:get");
