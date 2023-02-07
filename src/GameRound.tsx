c2dcbdc58d29b41ee936f430a0b60763f88249db
Ian Macartney <ian@convex.dev>
2023-02-07 13:56:13 -0800

    show list after submitting

--- src/GameRound.tsx
+++ src/GameRound.tsx
@@ -50,6 +50,18 @@ const GameRound: React.FC<{ roundId: Id<"rounds"> }> = ({ roundId }) => {
           <img src={round.imageUrl} />
           {round.mine ? (
             "This was your image"
+          ) : round.submitted.find((submission) => submission.me) ? (
+            <section>
+              You submitted!
+              <ul>
+                {round.submitted.map((player) => (
+                  <li key={player.pictureUrl}>
+                    <img src={player.pictureUrl} />
+                    {player.name} ✅
+                  </li>
+                ))}
+              </ul>
+            </section>
           ) : (
             <form
               onSubmit={async (e) => {
