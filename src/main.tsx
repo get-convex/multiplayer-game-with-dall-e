c8ae981d260c1d3d88d1a5a46735a1064719cc4f
Mike Wheaton <mike@wheaton.dev>
2023-02-15 15:13:36 -0600

    Install Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { SessionProvider } from "./hooks/sessionsClient";
 import "./styles/global.css";
+import "./styles/index.css";
 
 const client = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
