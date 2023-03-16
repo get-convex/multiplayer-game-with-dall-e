ee299bbf3a007184679ca840fa43c3f5e592ddd8
Ian Macartney <ian@convex.dev>
2023-03-16 12:12:35 -0700

    improve reveal page

--- src/RevealStage.tsx
+++ src/RevealStage.tsx
@@ -19,39 +19,52 @@ export function RevealStage({
 }) {
   return (
     <div className="flex flex-col">
-      <span className="my-4">Loading next prompt...</span>
-      <span className="text-xl mb-4">Reveal</span>
-      <ul className="border-t border-t-neutral-500">
+      <img
+        src={round.imageUrl}
+        alt=""
+        className="w-full max-w-xl border border-neutral-600 rounded overflow-hidden my-4"
+      />
+      <ul className="">
         {round.results.map((option) => (
           <li
             key={option.authorId}
             className="border-b border-b-neutral-500 py-4 flex flex-col items-start gap-2"
           >
-            {round.authorId === option.authorId && (
-              <span className="bg-green-300 rounded-full px-2 text-neutral-black">
-                Actual answer
-              </span>
-            )}
-            <span className="text-xl font-bold">{option.prompt}</span>
+            <span className="text-xl font-bold">
+              {option.prompt}{" "}
+              {round.authorId === option.authorId && (
+                <span className="font-normal bg-green-300 rounded-full px-2 text-neutral-black">
+                  Actual answer
+                </span>
+              )}
+            </span>
             <div className="flex gap-2">
               by {round.users.get(option.authorId)!.name}
-              <span className="rounded-full px-2 bg-orange-400 text-neutral-black">
-                +{option.scoreDeltas.get(option.authorId)}
-              </span>
+              {!!option.scoreDeltas.get(option.authorId) && (
+                <span className="rounded-full px-2 bg-orange-400 text-neutral-black">
+                  +{option.scoreDeltas.get(option.authorId)}
+                </span>
+              )}
             </div>
             {option.votes.length ? (
               <div className="pl-6">
                 <span className="text-sm font-bold">
-                  {option.votes.length} Votes
+                  {option.votes.length} Vote{option.votes.length > 1 && "s"}{" "}
                 </span>
                 <ol>
                   {option.votes.map((userId) => (
-                    <li key={userId}>
-                      {round.users.get(userId)!.name}
+                    <li key={userId} className="flex">
+                      <img
+                        src={round.users.get(userId)!.pictureUrl}
+                        width="24"
+                        height="24"
+                        className="rounded mr-1"
+                      />
+                      {round.users.get(userId)!.name || "(Anonymous)"}
 
                       {option.scoreDeltas.has(userId) ? (
-                        <span className="px-2 rounded-full bg-purple-400 text-neutral-black">
-                          {option.scoreDeltas.get(userId)}
+                        <span className="mx-1 px-2 rounded-full bg-purple-400 text-neutral-black">
+                          +{option.scoreDeltas.get(userId)}
                         </span>
                       ) : null}
                     </li>
