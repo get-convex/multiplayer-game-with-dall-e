dc02a8bd77e2e6cdc9aabd25e05bb704df56416f
Ian Macartney <ian@convex.dev>
2023-03-15 18:56:16 -0700

    pick buttons, see buttons when guessing

--- src/GameRound.tsx
+++ src/GameRound.tsx
@@ -73,12 +73,40 @@ const GameRound: React.FC<{ roundId: Id<"rounds"> }> = ({ roundId }) => {
             alt=""
             className="w-full max-w-xl border border-neutral-600 rounded overflow-hidden my-4"
           />
-          {round.mine ? (
-            "This was your image"
-          ) : round.submitted.find((submission) => submission.me) ? (
-            <section className="flex flex-col">
-              <span className="mb-2">Revealing answers...</span>
-              <span className="text-xl mb-4">You submitted!</span>
+          <fieldset>
+            <legend className="text-2xl mb-2">
+              {round.mine
+                ? "This was your image."
+                : "What text prompt was responsible for this image?"}
+            </legend>
+            <ul className="mb-6">
+              {round.options.map((option) => (
+                <li key={option} className="mb-2">
+                  <label className="flex gap-2 items-center text-lg">
+                    {error}
+                    <button
+                      onClick={async () => {
+                        const result = await submitGuess({
+                          roundId,
+                          prompt: option,
+                        });
+                        if (!result.success) setError(result.reason);
+                      }}
+                      disabled={round.mine || option === round.myPrompt}
+                      className="border border-blue-200 text-lg py-2 px-4 disabled:border-neutral-400 disabled:text-neutral-400 disabled:cursor-not-allowed cursor-pointer text-blue-200 hover:text-blue-400 hover:border-blue-400 transition-colors"
+                      aria-invalid={option === round.myGuess && !!error}
+                    >
+                      {option}
+                    </button>
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </fieldset>
+          {(round.mine ||
+            round.submitted.find((submission) => submission.me)) && (
+            <fieldset>
+              <legend className="text-2xl mb-2">Submissions</legend>
               <ul>
                 {round.submitted.map((player) => (
                   <li
@@ -93,33 +121,6 @@ const GameRound: React.FC<{ roundId: Id<"rounds"> }> = ({ roundId }) => {
                     />
                     <span className="text-lg">{player.name}</span>
                     {/* TODO: Replace emoji with icon. */}
-                  </li>
-                ))}
-              </ul>
-            </section>
-          ) : (
-            <fieldset>
-              <legend className="text-2xl mb-2">Guess the prompt</legend>
-              <ul className="mb-6">
-                {round.options.map((option) => (
-                  <li key={option} className="mb-2">
-                    <label className="flex gap-2 items-center text-lg">
-                      {error}
-                      <button
-                        onClick={async () => {
-                          const result = await submitGuess({
-                            roundId,
-                            prompt: option,
-                          });
-                          if (!result.success) setError(result.reason);
-                        }}
-                        disabled={option === round.myPrompt}
-                        className="border border-blue-200 text-lg py-2 px-4 disabled:border-neutral-400 disabled:text-neutral-400 disabled:cursor-not-allowed cursor-pointer text-blue-200 hover:text-blue-400 hover:border-blue-400 transition-colors"
-                        aria-invalid={option === round.myGuess && !!error}
-                      >
-                        {option}
-                      </button>
-                    </label>
                   </li>
                 ))}
               </ul>
