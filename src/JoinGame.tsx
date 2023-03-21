413caf9cd828b34c2128df8cec6775b0aef3f015
Ian Macartney <ian@convex.dev>
2023-03-20 18:39:56 -0700

    make join wider

--- src/JoinGame.tsx
+++ src/JoinGame.tsx
@@ -5,7 +5,7 @@ export function JoinGame(props: { gameCode: string }) {
   return (
     <div>
       <button
-        className="h-12 border border-blue-200 bg-blue-200 py-2 px-4 text-neutral-black hover:bg-blue-400"
+        className="h-12 w-48 border border-blue-200 bg-blue-200 py-2 px-4 text-neutral-black hover:bg-blue-400 rounded"
         onClick={() => joinGame(props.gameCode)}
       >
         Join
