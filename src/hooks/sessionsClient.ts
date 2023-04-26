68c48b2d28db6d011766541d8ff86e68d526b6d2
Ian Macartney <ian@convex.dev>
2023-04-25 21:04:32 -0700

    workign on 0.13 except md5

--- src/hooks/sessionsClient.ts
+++ src/hooks/sessionsClient.ts
@@ -12,14 +12,7 @@
  *
  * With the `SessionProvider` inside the `ConvexProvider` but outside your app.
  */
-import {
-  ActionNames,
-  MutationNames,
-  NamedAction,
-  NamedMutation,
-  NamedQuery,
-  QueryNames,
-} from "convex/browser";
+import { NamedAction, NamedMutation, NamedQuery } from "convex/browser";
 import React, { useContext, useEffect, useState } from "react";
 import { API } from "../../convex/_generated/api";
 import { Id } from "../../convex/_generated/dataModel";
@@ -46,13 +39,13 @@ export const SessionProvider: React.FC<{
   storageLocation?: "localStorage" | "sessionStorage";
   children?: React.ReactNode;
 }> = ({ storageLocation, children }) => {
-  const store = window[storageLocation ?? "sessionStorage"];
-  const [sessionId, setSession] = useState<Id<"sessions"> | null>(() => {
+  const store =
     // If it's rendering in SSR or such.
-    if (typeof window === "undefined") {
-      return null;
-    }
-    const stored = store.getItem(StoreKey);
+    typeof window === "undefined"
+      ? null
+      : window[storageLocation ?? "sessionStorage"];
+  const [sessionId, setSession] = useState<Id<"sessions"> | null>(() => {
+    const stored = store?.getItem(StoreKey);
     if (stored) {
       return new Id("sessions", stored);
     }
@@ -63,7 +56,7 @@ export const SessionProvider: React.FC<{
   // Get or set the ID from our desired storage location, whenever it changes.
   useEffect(() => {
     if (sessionId) {
-      store.setItem(StoreKey, sessionId.id);
+      store?.setItem(StoreKey, sessionId.id);
     } else {
       void (async () => {
         setSession(await createSession());
@@ -78,48 +71,75 @@ export const SessionProvider: React.FC<{
   );
 };
 
-type SessionFunction<Args extends any[]> = (
-  sessionId: Id<"sessions"> | null,
-  ...args: Args
+/**
+ * From ObjectType, pick the properties that are assignable to T.
+ */
+declare type PickByValue<ObjectType, T> = Pick<
+  ObjectType,
+  {
+    [Key in keyof ObjectType]: ObjectType[Key] extends T ? Key : never;
+  }[keyof ObjectType]
+>;
+
+/**
+ * Hack! This type causes TypeScript to simplify how it renders object types.
+ *
+ * It is functionally the identity for object types, but in practice it can
+ * simplify expressions like `A & B`.
+ */
+declare type Expand<ObjectType extends Record<any, any>> =
+  ObjectType extends Record<any, any>
+    ? {
+        [Key in keyof ObjectType]: ObjectType[Key];
+      }
+    : never;
+
+/**
+ * An `Omit<>` type that:
+ * 1. Applies to each element of a union.
+ * 2. Preserves the index signature of the underlying type.
+ */
+declare type BetterOmit<T, K extends keyof T> = {
+  [Property in keyof T as Property extends K ? never : Property]: T[Property];
+};
+
+type SessionFunction<Args extends object> = (
+  args: { sessionId: Id<"sessions"> | null } & Args
 ) => any;
 
-type SessionFunctionArgs<Fn extends SessionFunction<any>> =
-  Fn extends SessionFunction<infer Args> ? Args : never;
+type SessionFunctionArgs<Fn extends SessionFunction<any>> = Expand<
+  BetterOmit<Parameters<Fn>[0], "sessionId">
+>;
 
 // All the valid mutations that take Id<"sessions"> | null as their first parameter.
-type ValidQueryNames = {
-  [Name in QueryNames<API>]: NamedQuery<API, Name> extends SessionFunction<
-    any[]
-  >
-    ? Name
-    : never;
-}[QueryNames<API>];
+type ValidQueryNames = keyof PickByValue<
+  API["publicQueries"],
+  SessionFunction<any>
+> &
+  string;
 
 // Like useQuery, but for a Query that takes a session ID.
 export const useSessionQuery = <Name extends ValidQueryNames>(
   name: Name,
-  ...args: SessionFunctionArgs<NamedQuery<API, Name>>
+  args?: SessionFunctionArgs<NamedQuery<API, Name>>
 ) => {
   const sessionId = useContext(SessionContext);
   // I'm sorry about this. We know that ...args are the arguments following
   // a Id<"sessions"> | null, so it should always work. It's hard for typescript,
   // go easy on the poor little inference machine. Also open to ideas about how
   // to do this correctly.
-  const newArgs = [sessionId, ...args] as unknown as Parameters<
+  const newArgs = { ...(args || {}), sessionId } as unknown as Parameters<
     NamedQuery<API, Name>
-  >;
-  return useQuery(name, ...newArgs);
+  >[0];
+  return useQuery(name, newArgs);
 };
 
 // All the valid mutations that take Id<"sessions"> | null as their first parameter.
-type ValidMutationNames = {
-  [Name in MutationNames<API>]: NamedMutation<
-    API,
-    Name
-  > extends SessionFunction<any[]>
-    ? Name
-    : never;
-}[MutationNames<API>];
+type ValidMutationNames = keyof PickByValue<
+  API["publicMutations"],
+  SessionFunction<any>
+> &
+  string;
 
 // Like useMutation, but for a Mutation that takes a session ID.
 export const useSessionMutation = <Name extends ValidMutationNames>(
@@ -128,47 +148,40 @@ export const useSessionMutation = <Name extends ValidMutationNames>(
   const sessionId = useContext(SessionContext);
   const originalMutation = useMutation(name);
   return (
-    ...args: SessionFunctionArgs<NamedMutation<API, Name>>
+    args?: SessionFunctionArgs<NamedMutation<API, Name>>
   ): Promise<ReturnType<NamedMutation<API, Name>>> => {
     // I'm sorry about this. We know that ...args are the arguments following
     // a Id<"sessions"> | null, so it should always work. It's hard for typescript,
     // go easy on the poor little inference machine. Also open to ideas about how
     // to do this correctly.
-    const newArgs = [sessionId, ...args] as unknown as Parameters<
+    const newArgs = { ...(args || {}), sessionId } as unknown as Parameters<
       NamedMutation<API, Name>
-    >;
-    return originalMutation(...newArgs);
+    >[0];
+    return originalMutation(newArgs);
   };
 };
 
-type SessionFunctionObj<Args extends {}> = (
-  args: {
-    sessionId: Id<"sessions"> | null;
-  } & Args
-) => any;
+// All the valid actions that take Id<"sessions"> | null as their first parameter.
+type ValidActionNames = keyof PickByValue<
+  API["publicActions"],
+  SessionFunction<any>
+> &
+  string;
 
-type SessionFunctionArgsObj<Fn extends SessionFunctionObj<any>> =
-  Fn extends SessionFunctionObj<infer Args> ? Omit<Args, "sessionId"> : never;
-
-// All the valid actions that take {sessionId: Id<"sessions"> | null}.
-type ValidActionNames = {
-  [Name in ActionNames<API>]: NamedAction<
-    API,
-    Name
-  > extends SessionFunctionObj<any>
-    ? Name
-    : never;
-}[ActionNames<API>];
-// Like useAction, but for an Action that takes a session ID.
-export const useSessionAction = <Name extends ActionNames<API>>(name: Name) => {
+// Like useAction, but for a Action that takes a session ID.
+export const useSessionAction = <Name extends ValidActionNames>(name: Name) => {
   const sessionId = useContext(SessionContext);
   const originalAction = useAction(name);
   return (
-    args: SessionFunctionArgsObj<NamedAction<API, Name>>
+    args?: SessionFunctionArgs<NamedAction<API, Name>>
   ): Promise<ReturnType<NamedAction<API, Name>>> => {
-    const newArgs = [
-      { sessionId, ...(args as object) },
-    ] as unknown as Parameters<NamedAction<API, Name>>;
-    return originalAction(...newArgs);
+    // I'm sorry about this. We know that ...args are the arguments following
+    // a Id<"sessions"> | null, so it should always work. It's hard for typescript,
+    // go easy on the poor little inference machine. Also open to ideas about how
+    // to do this correctly.
+    const newArgs = { ...(args || {}), sessionId } as unknown as Parameters<
+      NamedAction<API, Name>
+    >[0];
+    return originalAction(newArgs);
   };
 };
