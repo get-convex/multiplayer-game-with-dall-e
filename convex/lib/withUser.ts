74a175185031adc21496029748511f4e09782e8f
Ian Macartney <ian@convex.dev>
2023-04-25 18:11:59 -0700

    withUser, but unused mostly

--- convex/lib/withUser.ts
+++ convex/lib/withUser.ts
@@ -10,19 +10,32 @@ import { Doc } from "../_generated/dataModel";
  * @param func - Your function that can now take in a `user` in the first param.
  * @returns A function to be passed to `query` or `mutation`.
  */
-export const withUser = <Ctx extends QueryCtx, Args extends any[], Output>(
-  func: (ctx: Ctx & { user: Doc<"users"> }, ...args: Args) => Promise<Output>
-): ((ctx: Ctx, ...args: Args) => Promise<Output>) => {
-  return async (ctx: Ctx, ...args: Args) => {
+export const withUser = <
+  Ctx extends QueryCtx,
+  Args extends Record<string, any>,
+  Output
+>(
+  func: (ctx: Ctx & { user: Doc<"users"> }, args: Args) => Promise<Output>
+): ((ctx: Ctx, args: Args) => Promise<Output>) => {
+  return async (ctx: Ctx, args: Args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new Error(
         "Unauthenticated call to function requiring authentication"
       );
     }
-    const user = await getUser(ctx.db, identity.tokenIdentifier);
+    // Note: If you don't want to define an index right away, you can use
+    // db.query("users")
+    //  .filter(q => q.eq(q.field("tokenIdentifier"), identity.tokenIdentifier))
+    //  .unique();
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
+      )
+      .unique();
     if (!user) throw new Error("User not found");
-    return func({ ...ctx, user }, ...args);
+    return func({ ...ctx, user }, args);
   };
 };
 
